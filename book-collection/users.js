4153acc59b745fa400100cbbd4ee6da638a22e95
haregewoyn-soramo <haregewoynkeno@gmail.com>
2024-04-26 16:57:56 -0600

    this is sba rest api

---
+++ book-collection/users.js
@@ -0,0 +1,37 @@
+const users = [
+  {
+    "ID": 1,
+    "Username": "user1",
+    "Email": "user1@example.com"
+  },
+  {
+    "ID": 2,
+    "Username": "user2",
+    "Email": "user2@example.com"
+  },
+  {
+    "ID": 3,
+    "Username": "user3",
+    "Email": "user3@example.com"
+  },
+  {
+    "ID": 4,
+    "Username": "user4",
+    "Email": "user4@example.com"
+  },
+  {
+    "ID": 5,
+    "Username": "user5",
+    "Email": "user5@example.com"
+  },
+  {
+    "ID": 6,
+    "Username": "user6",
+    "Email": "user6@example.com"
+  },
+  {
+    "ID": 7,
+    "Username": "user7",
+    "Email": "user7@example.com"
+  },
+]
